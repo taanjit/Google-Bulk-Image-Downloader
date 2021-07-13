b4638e1eece885364f32c5ab29a1e671d454e080
Aishwarya Prasad <aishwarya@perfit.today>
2021-07-13 12:33:27 +0530

    initial push

---
+++ Bulk_image_download_from_google_images.py
@@ -0,0 +1,5 @@
+from simple_image_download import simple_image_download as simp
+
+response = simp.simple_image_download
+
+response().download(keywords="Amazon Brand - Vedaka Premium Toor Dal",limit=50)
